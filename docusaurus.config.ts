09093aa74f7e1433261d43e373ba7a19fe5b59e2
pak1ga <khajanchandra.pandey@mx.bosch.com>
2024-07-07 11:01:21 -0600

    deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'iamkhajan', // Usually your GitHub org/user name.
   projectName: 'iamkhajan.github.io', // Usually your repo name.
+  deploymentBranch:'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
