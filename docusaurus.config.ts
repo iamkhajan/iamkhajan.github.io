a6b12253537e03c57cdcc1c9ff948d676ffa9caa
Khajan Pandey <khajanpandey@Khajans-MacBook-Pro.local>
2026-02-07 19:32:12 -0800

    chore: setup custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://iamkhajan.github.io',
+  // url: 'https://iamkhajan.github.io',
+  url: 'https://khajanpandey.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
