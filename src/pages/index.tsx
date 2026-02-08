05c9b446f7868fb0027b6a169db7fa76a8be4155
Khajan Pandey <khajanpandey@Khajans-MacBook-Pro.local>
2026-02-07 16:56:08 -0800

    social media addition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'; // Import icons
+
 
 import styles from './index.module.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
@@ -30,6 +32,20 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
+        
+        {/* SOCIAL MEDIA ROW */}
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', fontSize: '2rem', marginBottom: '1.5rem' }}>
+          <a href="https://www.linkedin.com/in/khajan-pandey-3b515035/" target="_blank" style={{ color: 'white' }}>
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/iamkhajan" target="_blank" style={{ color: 'white' }}>
+            <FaGithub />
+          </a>
+          <a href="mailto:khajanpandey@outlook.com" style={{ color: 'white' }}>
+            <FaEnvelope />
+          </a>
+        </div>
+
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
