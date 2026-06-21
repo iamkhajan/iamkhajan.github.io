6f5bea31b86506a33d17f2f59b577d1b6b372ea9
Khajan Pandey <khajanpandey@Khajans-MacBook-Pro.local>
2026-06-21 02:14:17 -0700

    metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,7 +81,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Bridging moments <head />">
       <HomepageHeader />
       {/* <main>
         <HomepageFeatures />
